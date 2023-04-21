2e706844d654cfc370f1ad7c8fa6dd729f1617b6
witteerik <erik.witte@oru.se>
2023-04-21 17:38:55 +0200

    Finished first version of program

--- VideoExtractionTool/VideoExtractionTool/MainForm.Designer.vb
+++ VideoExtractionTool/VideoExtractionTool/MainForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MainForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,175 @@ Partial Class MainForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Info_RichTextBox = New System.Windows.Forms.RichTextBox()
+        Me.ffmpegPath_TextBox = New System.Windows.Forms.TextBox()
+        Me.Data_RichTextBox = New System.Windows.Forms.RichTextBox()
+        Me.Start_Button = New System.Windows.Forms.Button()
+        Me.ShowProcessWindow_CheckBox = New System.Windows.Forms.CheckBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Process_ToolStripStatusLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Label1, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label3, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Info_RichTextBox, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.ffmpegPath_TextBox, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Data_RichTextBox, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Start_Button, 0, 5)
+        Me.TableLayoutPanel1.Controls.Add(Me.ShowProcessWindow_CheckBox, 0, 4)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 6
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 138.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 67.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(807, 470)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Location = New System.Drawing.Point(3, 138)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(147, 30)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ffmpeg.exe file path:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label3.Location = New System.Drawing.Point(3, 168)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(147, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Input data"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft
+        '
+        'Info_RichTextBox
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Info_RichTextBox, 2)
+        Me.Info_RichTextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Info_RichTextBox.Location = New System.Drawing.Point(3, 3)
+        Me.Info_RichTextBox.Name = "Info_RichTextBox"
+        Me.Info_RichTextBox.ReadOnly = True
+        Me.Info_RichTextBox.Size = New System.Drawing.Size(801, 132)
+        Me.Info_RichTextBox.TabIndex = 4
+        Me.Info_RichTextBox.Text = resources.GetString("Info_RichTextBox.Text")
+        '
+        'ffmpegPath_TextBox
+        '
+        Me.ffmpegPath_TextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ffmpegPath_TextBox.Location = New System.Drawing.Point(156, 141)
+        Me.ffmpegPath_TextBox.Name = "ffmpegPath_TextBox"
+        Me.ffmpegPath_TextBox.Size = New System.Drawing.Size(648, 23)
+        Me.ffmpegPath_TextBox.TabIndex = 5
+        '
+        'Data_RichTextBox
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Data_RichTextBox, 2)
+        Me.Data_RichTextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Data_RichTextBox.Location = New System.Drawing.Point(3, 191)
+        Me.Data_RichTextBox.Name = "Data_RichTextBox"
+        Me.Data_RichTextBox.Size = New System.Drawing.Size(801, 184)
+        Me.Data_RichTextBox.TabIndex = 6
+        Me.Data_RichTextBox.Text = ""
+        '
+        'Start_Button
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.Start_Button, 2)
+        Me.Start_Button.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Start_Button.Location = New System.Drawing.Point(3, 406)
+        Me.Start_Button.Name = "Start_Button"
+        Me.Start_Button.Size = New System.Drawing.Size(801, 61)
+        Me.Start_Button.TabIndex = 7
+        Me.Start_Button.Text = "Start extraction process"
+        Me.Start_Button.UseVisualStyleBackColor = True
+        '
+        'ShowProcessWindow_CheckBox
+        '
+        Me.ShowProcessWindow_CheckBox.AutoSize = True
+        Me.ShowProcessWindow_CheckBox.Location = New System.Drawing.Point(3, 381)
+        Me.ShowProcessWindow_CheckBox.Name = "ShowProcessWindow_CheckBox"
+        Me.ShowProcessWindow_CheckBox.Size = New System.Drawing.Size(143, 19)
+        Me.ShowProcessWindow_CheckBox.TabIndex = 8
+        Me.ShowProcessWindow_CheckBox.Text = "Show process window"
+        Me.ShowProcessWindow_CheckBox.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Process_ToolStripStatusLabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 470)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.StatusStrip1.Size = New System.Drawing.Size(807, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "Idle"
+        '
+        'Process_ToolStripStatusLabel
+        '
+        Me.Process_ToolStripStatusLabel.Name = "Process_ToolStripStatusLabel"
+        Me.Process_ToolStripStatusLabel.Size = New System.Drawing.Size(26, 17)
+        Me.Process_ToolStripStatusLabel.Text = "Idle"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.TableLayoutPanel1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(807, 470)
+        Me.Panel1.TabIndex = 2
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "MainForm"
+        Me.ClientSize = New System.Drawing.Size(807, 492)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Name = "MainForm"
+        Me.Text = "Video extraction tool"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Info_RichTextBox As RichTextBox
+    Friend WithEvents ffmpegPath_TextBox As TextBox
+    Friend WithEvents Data_RichTextBox As RichTextBox
+    Friend WithEvents Start_Button As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Process_ToolStripStatusLabel As ToolStripStatusLabel
+    Friend WithEvents ShowProcessWindow_CheckBox As CheckBox
 End Class
--- VideoExtractionTool/VideoExtractionTool/MainForm.vb
+++ VideoExtractionTool/VideoExtractionTool/MainForm.vb
@@ -1,3 +1,222 @@
-﻿Public Class MainForm
+﻿Imports System.Globalization
+
+Public Class MainForm
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        SetStartButtonLook(StartButtonLooks.Start)
+
+    End Sub
+
+    Private Sub Start_Button_Click(sender As Object, e As EventArgs) Handles Start_Button.Click
+
+        Try
+
+            'Checking that the ffmpeg.exe file exists
+            Dim ffmpegPath = ffmpegPath_TextBox.Text.Trim
+            If ffmpegPath.Trim = "" Then
+                MsgBox("You must indicate the path to an ffmpeg.exe file!", MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                Exit Sub
+            End If
+            If IO.File.Exists(ffmpegPath) = False Then
+                MsgBox("The supplied path to the ffmpeg.exe file does not exist!", MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                Exit Sub
+            End If
+
+            'Getting data
+            Dim ExtractionData As New List(Of Tuple(Of String, String, Double, Double))
+
+            Dim OutputCheckList As New SortedSet(Of String)
+
+            Dim DataLines = Data_RichTextBox.Lines
+
+            For Each DataLine In DataLines
+
+                'Skipping empty lines
+                If DataLine.Trim = "" Then Continue For
+
+                'Checking that there are four tab-delimited items on the line
+                Dim LineCols = DataLine.Trim.Split(vbTab)
+                If LineCols.Length <> 4 Then
+                    MsgBox("The following line does not have four tab-delimited values (as required)!" & vbCrLf & vbCrLf & DataLine, MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                    Exit Sub
+                End If
+
+                Dim InputPath = LineCols(0).Trim
+                Dim OutputPath = LineCols(1).Trim
+                Dim StartTimeString = LineCols(2).Trim
+                Dim DurationString = LineCols(3).Trim
+                Dim StartTime As Double
+                Dim Duration As Double
+
+                'Parsing and checking values
+                If IO.File.Exists(InputPath) = False Then
+                    MsgBox("The input file indicated on the following line does not exist!" & vbCrLf & vbCrLf & DataLine, MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                    Exit Sub
+                End If
+                If Double.TryParse(StartTimeString.Replace(",", "."), NumberStyles.Float, CultureInfo.InvariantCulture, StartTime) = False Then
+                    MsgBox("Unable to parse the start time on the following line!" & vbCrLf & vbCrLf & DataLine, MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                    Exit Sub
+                End If
+                If Double.TryParse(DurationString.Replace(",", "."), NumberStyles.Float, CultureInfo.InvariantCulture, Duration) = False Then
+                    MsgBox("Unable to parse the duration on the following line!" & vbCrLf & vbCrLf & DataLine, MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                    Exit Sub
+                End If
+
+                'Checks that output files are unique
+                If OutputCheckList.Contains(OutputPath) Then
+                    MsgBox("The output file indicated on the following line is listed more than once!" & vbCrLf & vbCrLf & DataLine, MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                    Exit Sub
+                Else
+                    OutputCheckList.Add(OutputPath)
+                End If
+
+                'Checks that the output file does not exist
+                If IO.File.Exists(OutputPath) Then
+                    MsgBox("The output file indicated on the following line already exist! Remove it and try again! (Overwriting files is not supported.)" & vbCrLf & vbCrLf & DataLine, MsgBoxStyle.Exclamation, "Video Extraction Tool")
+                    Exit Sub
+                End If
+
+                ExtractionData.Add(New Tuple(Of String, String, Double, Double)(InputPath, OutputPath, StartTime, Duration))
+
+            Next
+
+            'Everything seems fine, changing the start button to stop mode
+            SetStartButtonLook(StartButtonLooks.Stop)
+
+            'Noting progress
+            Dim ProcessedItems As Integer = 0
+            UpdateProgressLabel(ProcessedItems, ExtractionData.Count)
+
+            Dim FailedItems As New List(Of String)
+
+            'Starting extraction
+            For Each ExtractionVideo In ExtractionData
+
+
+                Dim InputPath = ExtractionVideo.Item1
+                Dim OutputPath = ExtractionVideo.Item2
+                Dim StartTime = ExtractionVideo.Item3
+                Dim Duration = ExtractionVideo.Item4
+
+                'Modifying the starttime and duration to account for the (rather odd) way of specifying times (or maybe I've gotten it wrong..., it seems to work though...)
+                Duration = Duration + StartTime
+                StartTime = -StartTime
+                If StartTime = -0 Then StartTime = 0
+
+                'Creating the output folder
+                IO.Directory.CreateDirectory(IO.Path.GetDirectoryName(OutputPath))
+
+                'Adding " characters to the paths to allow empty spaces in them
+                InputPath = Chr(34) & InputPath & Chr(34)
+                OutputPath = Chr(34) & OutputPath & Chr(34)
+
+                Dim StartTimeString As String = StartTime.ToString.Replace(",", ".")
+                Dim DurationString As String = Duration.ToString.Replace(",", ".")
+
+                'Creating resampled file
+                Dim ffmpegProcessStartInfo As New ProcessStartInfo()
+                ffmpegProcessStartInfo.FileName = ffmpegPath
+                If StartTime = 0 Then
+                    ffmpegProcessStartInfo.Arguments = "-i " & InputPath & " -c copy " & " -t " & DurationString & " " & OutputPath
+                Else
+                    ffmpegProcessStartInfo.Arguments = "-i " & InputPath & " -c copy -ss " & StartTimeString & " -t " & DurationString & " " & OutputPath
+                End If
+
+                If ShowProcessWindow_CheckBox.Checked = False Then
+                    ffmpegProcessStartInfo.CreateNoWindow = True
+                End If
+
+                Dim sp = Process.Start(ffmpegProcessStartInfo)
+                sp.WaitForExit()
+
+                If sp.ExitCode = 1 Then
+                    FailedItems.Add(InputPath & vbTab & OutputPath & vbTab & StartTimeString & vbTab & DurationString)
+                End If
+
+                sp.Close()
+
+                ProcessedItems += 1
+                UpdateProgressLabel(ProcessedItems, ExtractionData.Count)
+
+                'Exits sub if the user pressed the stop button
+                If CurrentStartButtonLook = StartButtonLooks.Start Then Exit Sub
+
+            Next
+
+            If FailedItems.Count > 0 Then
+                'Error message
+                Dim ErrorForm As New Windows.Forms.Form
+                ErrorForm.Text = "Failed video extractions"
+                ErrorForm.Height = 300
+                ErrorForm.Width = 500
+
+                Dim FailedItemCount As Integer = FailedItems.Count
+                FailedItems.Insert(0, "The following " & FailedItemCount & " items could not be extracted:")
+                FailedItems.Add("")
+                FailedItems.Add("Close this window to continue!")
+
+                Dim ErrorTextBox As New RichTextBox With {.Dock = DockStyle.Fill, .Lines = FailedItems.ToArray}
+                ErrorForm.Controls.Add(ErrorTextBox)
+
+                ErrorForm.ShowDialog()
+            Else
+                MsgBox("Finished extracting all videos.", MsgBoxStyle.Information, "Video Extraction Tool")
+            End If
+
+        Catch ex As Exception
+
+            MsgBox("The following error occurred:" & vbCrLf & ex.ToString, MsgBoxStyle.Exclamation, "Video Extraction Tool")
+
+        End Try
+
+        'Resetting things
+        ResetProgressLabel()
+        SetStartButtonLook(StartButtonLooks.Start)
+
+    End Sub
+
+    Private Sub UpdateProgressLabel(ByVal ProcessedItems As Integer, ByVal TotalCount As Integer)
+
+        Process_ToolStripStatusLabel.Text = "Processing " & ProcessedItems & " / " & TotalCount
+        Process_ToolStripStatusLabel.Invalidate()
+        Me.Update()
+
+    End Sub
+
+    Private Sub ResetProgressLabel()
+
+        Process_ToolStripStatusLabel.Text = "Idle"
+        Process_ToolStripStatusLabel.Invalidate()
+
+    End Sub
+
+    Private Enum StartButtonLooks
+        Start
+        [Stop]
+    End Enum
+
+    Private CurrentStartButtonLook As StartButtonLooks
+
+    Private Sub SetStartButtonLook(ByVal Look As StartButtonLooks)
+
+        CurrentStartButtonLook = Look
+
+        Select Case Look
+            Case StartButtonLooks.Start
+
+                Start_Button.Text = "Start extraction process"
+                Start_Button.Invalidate()
+                Start_Button.Update()
+
+            Case StartButtonLooks.Stop
+
+                Start_Button.Text = "Stop processing"
+                Start_Button.Invalidate()
+                Start_Button.Update()
+
+        End Select
+
+    End Sub
 
 End Class
